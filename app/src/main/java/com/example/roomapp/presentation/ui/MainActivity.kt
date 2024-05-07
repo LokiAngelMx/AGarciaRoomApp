92399886fa684c658bd2b6e4efe503151658effa
LokiAngelMx <LokiGarciaMx@gmail.com>
2024-05-07 14:02:29 -0600

    Observe state

--- app/src/main/java/com/example/roomapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/roomapp/presentation/ui/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.roomapp.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.roomapp.R
 import com.example.roomapp.domain.models.Hero
 import com.example.roomapp.presentation.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,5 +26,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        observeState()
+    }
+
+    private fun observeState(){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                mainViewModel.state.collect{
+                    Log.i("Heroes", it.heroes.toString())
+                }
+            }
+        }
     }
 }
